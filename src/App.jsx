53f8643cbb8ebf1f73eef6d76d58b39bc223ff34
Saba Kevlishvili <saba@theneo.io>
2023-11-21 23:18:31 +0400

    guitar sim

--- src/App.jsx
+++ src/App.jsx
@@ -97,6 +97,38 @@ function GuitarString({ position, length = 290, audioPath }) {
   };
 
   useEffect(() => {
+    var ctx = null,
+      usingWebAudio = true;
+
+    try {
+      if (typeof AudioContext !== "undefined") {
+        ctx = new AudioContext();
+      } else if (typeof webkitAudioContext !== "undefined") {
+        ctx = new webkitAudioContext();
+      } else {
+        usingWebAudio = false;
+      }
+    } catch (e) {
+      usingWebAudio = false;
+    }
+
+    // context state at this time is `undefined` in iOS8 Safari
+    if (usingWebAudio && ctx.state === "suspended") {
+      var resume = function () {
+        ctx.resume();
+
+        setTimeout(function () {
+          if (ctx.state === "running") {
+            document.body.removeEventListener("touchend", resume, false);
+          }
+        }, 0);
+      };
+
+      document.body.addEventListener("touchend", resume, false);
+    }
+  }, []);
+
+  useEffect(() => {
     const context = new AudioContext();
     setAudioContext(context);
 
@@ -152,10 +184,6 @@ function GuitarString({ position, length = 290, audioPath }) {
     uniforms.amplitude.value = amplitude;
     uniforms.time.value = clock.getElapsedTime();
   });
-
-  const handleEndInteraction = () => {
-    set(false);
-  };
 
   return (
     <mesh
