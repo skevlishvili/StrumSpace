fc57f5810341cf651778ed9b0d254f37b1219c1c
Saba Kevlishvili <saba@theneo.io>
2023-11-22 00:25:06 +0400

    guitar sim

--- src/App.jsx
+++ src/App.jsx
@@ -142,6 +142,32 @@ function App({ ...props }) {
   const [start, setStart] = useState(false);
   const [lock, setLock] = useState(false);
 
+  useEffect(() => {
+    // Create an audio context instance if WebAudio is supported
+    let context =
+      window.AudioContext || window.webkitAudioContext
+        ? new (window.AudioContext || window.webkitAudioContext)()
+        : null;
+
+    // Decide on some parameters
+    let allowBackgroundPlayback = false; // default false, recommended false
+    let forceIOSBehavior = false; // default false, recommended false
+    // Pass it to unmute if the context exists... ie WebAudio is supported
+    if (context) {
+      // If you need to be able to disable unmute at a later time, you can use the returned handle's dispose() method
+      // if you don't need to do that (most folks won't) then you can simply ignore the return value
+      let unmuteHandle = unmute(
+        context,
+        allowBackgroundPlayback,
+        forceIOSBehavior
+      );
+
+      // ... at some later point you wish to STOP unmute control
+      unmuteHandle.dispose();
+      unmuteHandle = null;
+    }
+  }, []);
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       {!start ? (
